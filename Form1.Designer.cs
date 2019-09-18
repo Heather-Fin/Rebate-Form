14efb164f4a9409515d6d2584a9627f05fb9590c
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-09-18 15:09:41 -0500

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Heather_Finnegan_Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_firstName = new System.Windows.Forms.Label();
+            this.lbl_middleInitial = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_firstName
+            // 
+            this.lbl_firstName.AutoSize = true;
+            this.lbl_firstName.Location = new System.Drawing.Point(12, 25);
+            this.lbl_firstName.Name = "lbl_firstName";
+            this.lbl_firstName.Size = new System.Drawing.Size(57, 13);
+            this.lbl_firstName.TabIndex = 0;
+            this.lbl_firstName.Text = "First Name";
+            // 
+            // lbl_middleInitial
+            // 
+            this.lbl_middleInitial.AutoSize = true;
+            this.lbl_middleInitial.Location = new System.Drawing.Point(15, 64);
+            this.lbl_middleInitial.Name = "lbl_middleInitial";
+            this.lbl_middleInitial.Size = new System.Drawing.Size(65, 13);
+            this.lbl_middleInitial.TabIndex = 1;
+            this.lbl_middleInitial.Text = "Middle Initial";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 408);
+            this.Controls.Add(this.lbl_middleInitial);
+            this.Controls.Add(this.lbl_firstName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_firstName;
+        private System.Windows.Forms.Label lbl_middleInitial;
+    }
+}
+
